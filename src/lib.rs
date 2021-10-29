b2b1329d92f6d22a985086cc9a267ce556bdc672
Frank Denis <github@pureftpd.org>
2021-10-29 12:44:53 +0200

    Vectorize

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 use core::convert::TryInto;
 use core::fmt;
+use core::ops::*;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Error {
@@ -34,6 +35,69 @@ struct Lane {
     l: [u64; 4],
 }
 
+impl BitXor for Lane {
+    type Output = Self;
+
+    #[inline]
+    fn bitxor(self, o: Self) -> Self {
+        Lane {
+            l: [
+                self.l[0] ^ o.l[0],
+                self.l[1] ^ o.l[1],
+                self.l[2] ^ o.l[2],
+                self.l[3] ^ o.l[3],
+            ],
+        }
+    }
+}
+
+impl BitXorAssign for Lane {
+    #[inline]
+    fn bitxor_assign(&mut self, o: Self) {
+        self.l[0] ^= o.l[0];
+        self.l[1] ^= o.l[1];
+        self.l[2] ^= o.l[2];
+        self.l[3] ^= o.l[3];
+    }
+}
+
+impl BitAnd for Lane {
+    type Output = Self;
+
+    #[inline]
+    fn bitand(self, o: Self) -> Self {
+        Lane {
+            l: [
+                self.l[0] & o.l[0],
+                self.l[1] & o.l[1],
+                self.l[2] & o.l[2],
+                self.l[3] & o.l[3],
+            ],
+        }
+    }
+}
+
+impl Lane {
+    #[inline]
+    fn rotate_left(self, o: u32) -> Self {
+        Lane {
+            l: [
+                self.l[0].rotate_left(o),
+                self.l[1].rotate_left(o),
+                self.l[2].rotate_left(o),
+                self.l[3].rotate_left(o),
+            ],
+        }
+    }
+
+    #[inline]
+    fn shuffle(self, a: usize, b: usize, c: usize, d: usize) -> Self {
+        Lane {
+            l: [self.l[a], self.l[b], self.l[c], self.l[d]],
+        }
+    }
+}
+
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy)]
 struct State {
@@ -41,107 +105,27 @@ struct State {
 }
 
 impl State {
-    fn update(&mut self, input: &Lane) {
+    fn update(&mut self, input: Lane) {
         let s = &mut self.s;
-        s[0].l[0] ^= s[3].l[0];
-        s[0].l[1] ^= s[3].l[1];
-        s[0].l[2] ^= s[3].l[2];
-        s[0].l[3] ^= s[3].l[3];
-        let t = s[3].l[3];
-        s[3].l[3] = s[3].l[2];
-        s[3].l[2] = s[3].l[1];
-        s[3].l[1] = s[3].l[0];
-        s[3].l[0] = t;
-        s[0].l[0] ^= s[1].l[0] & s[2].l[0];
-        s[0].l[1] ^= s[1].l[1] & s[2].l[1];
-        s[0].l[2] ^= s[1].l[2] & s[2].l[2];
-        s[0].l[3] ^= s[1].l[3] & s[2].l[3];
-        s[0].l[0] = s[0].l[0].rotate_left(13);
-        s[0].l[1] = s[0].l[1].rotate_left(13);
-        s[0].l[2] = s[0].l[2].rotate_left(13);
-        s[0].l[3] = s[0].l[3].rotate_left(13);
+        s[0] ^= s[3] ^ (s[1] & s[2]);
+        s[0] = s[0].rotate_left(13);
+        s[3] = s[3].shuffle(3, 0, 1, 2);
 
-        s[1].l[0] ^= input.l[0];
-        s[1].l[1] ^= input.l[1];
-        s[1].l[2] ^= input.l[2];
-        s[1].l[3] ^= input.l[3];
-        s[1].l[0] ^= s[4].l[0];
-        s[1].l[1] ^= s[4].l[1];
-        s[1].l[2] ^= s[4].l[2];
-        s[1].l[3] ^= s[4].l[3];
-        s[4].l.swap(3, 1);
-        s[4].l.swap(2, 0);
-        s[1].l[0] ^= s[2].l[0] & s[3].l[0];
-        s[1].l[1] ^= s[2].l[1] & s[3].l[1];
-        s[1].l[2] ^= s[2].l[2] & s[3].l[2];
-        s[1].l[3] ^= s[2].l[3] & s[3].l[3];
-        s[1].l[0] = s[1].l[0].rotate_left(46);
-        s[1].l[1] = s[1].l[1].rotate_left(46);
-        s[1].l[2] = s[1].l[2].rotate_left(46);
-        s[1].l[3] = s[1].l[3].rotate_left(46);
+        s[1] ^= input ^ s[4] ^ (s[2] & s[3]);
+        s[1] = s[1].rotate_left(46);
+        s[4] = s[4].shuffle(2, 3, 0, 1);
 
-        s[2].l[0] ^= input.l[0];
-        s[2].l[1] ^= input.l[1];
-        s[2].l[2] ^= input.l[2];
-        s[2].l[3] ^= input.l[3];
-        s[2].l[0] ^= s[0].l[0];
-        s[2].l[1] ^= s[0].l[1];
-        s[2].l[2] ^= s[0].l[2];
-        s[2].l[3] ^= s[0].l[3];
-        let t = s[0].l[0];
-        s[0].l[0] = s[0].l[1];
-        s[0].l[1] = s[0].l[2];
-        s[0].l[2] = s[0].l[3];
-        s[0].l[3] = t;
-        s[2].l[0] ^= s[3].l[0] & s[4].l[0];
-        s[2].l[1] ^= s[3].l[1] & s[4].l[1];
-        s[2].l[2] ^= s[3].l[2] & s[4].l[2];
-        s[2].l[3] ^= s[3].l[3] & s[4].l[3];
-        s[2].l[0] = s[2].l[0].rotate_left(38);
-        s[2].l[1] = s[2].l[1].rotate_left(38);
-        s[2].l[2] = s[2].l[2].rotate_left(38);
-        s[2].l[3] = s[2].l[3].rotate_left(38);
+        s[2] ^= input ^ s[0] ^ (s[3] & s[4]);
+        s[2] = s[2].rotate_left(38);
+        s[0] = s[0].shuffle(1, 2, 3, 0);
 
-        s[3].l[0] ^= input.l[0];
-        s[3].l[1] ^= input.l[1];
-        s[3].l[2] ^= input.l[2];
-        s[3].l[3] ^= input.l[3];
-        s[3].l[0] ^= s[1].l[0];
-        s[3].l[1] ^= s[1].l[1];
-        s[3].l[2] ^= s[1].l[2];
-        s[3].l[3] ^= s[1].l[3];
-        s[1].l.swap(3, 1);
-        s[1].l.swap(2, 0);
-        s[3].l[0] ^= s[4].l[0] & s[0].l[0];
-        s[3].l[1] ^= s[4].l[1] & s[0].l[1];
-        s[3].l[2] ^= s[4].l[2] & s[0].l[2];
-        s[3].l[3] ^= s[4].l[3] & s[0].l[3];
-        s[3].l[0] = s[3].l[0].rotate_left(7);
-        s[3].l[1] = s[3].l[1].rotate_left(7);
-        s[3].l[2] = s[3].l[2].rotate_left(7);
-        s[3].l[3] = s[3].l[3].rotate_left(7);
+        s[3] ^= input ^ s[1] ^ (s[4] & s[0]);
+        s[3] = s[3].rotate_left(7);
+        s[1] = s[1].shuffle(2, 3, 0, 1);
 
-        s[4].l[0] ^= input.l[0];
-        s[4].l[1] ^= input.l[1];
-        s[4].l[2] ^= input.l[2];
-        s[4].l[3] ^= input.l[3];
-        s[4].l[0] ^= s[2].l[0];
-        s[4].l[1] ^= s[2].l[1];
-        s[4].l[2] ^= s[2].l[2];
-        s[4].l[3] ^= s[2].l[3];
-        let t = s[2].l[3];
-        s[2].l[3] = s[2].l[2];
-        s[2].l[2] = s[2].l[1];
-        s[2].l[1] = s[2].l[0];
-        s[2].l[0] = t;
-        s[4].l[0] ^= s[0].l[0] & s[1].l[0];
-        s[4].l[1] ^= s[0].l[1] & s[1].l[1];
-        s[4].l[2] ^= s[0].l[2] & s[1].l[2];
-        s[4].l[3] ^= s[0].l[3] & s[1].l[3];
-        s[4].l[0] = s[4].l[0].rotate_left(4);
-        s[4].l[1] = s[4].l[1].rotate_left(4);
-        s[4].l[2] = s[4].l[2].rotate_left(4);
-        s[4].l[3] = s[4].l[3].rotate_left(4);
+        s[4] ^= input ^ s[2] ^ (s[0] & s[1]);
+        s[4] = s[4].rotate_left(4);
+        s[2] = s[2].shuffle(3, 0, 1, 2);
     }
 
     pub fn new(key: &Key, nonce: &Nonce) -> Self {
@@ -180,7 +164,7 @@ impl State {
             ],
         };
         for _ in 0..16 {
-            state.update(&Lane { l: [0u64; 4] });
+            state.update(Lane { l: [0u64; 4] });
         }
         state.s[1].l[0] ^= k0;
         state.s[1].l[1] ^= k1;
@@ -213,7 +197,7 @@ impl State {
         dst[8..16].copy_from_slice(&c.l[1].to_le_bytes());
         dst[16..24].copy_from_slice(&c.l[2].to_le_bytes());
         dst[24..32].copy_from_slice(&c.l[3].to_le_bytes());
-        self.update(&p);
+        self.update(p);
     }
 
     fn dec(&mut self, dst: &mut [u8; 32], src: &[u8; 32]) {
@@ -240,7 +224,7 @@ impl State {
         dst[8..16].copy_from_slice(&p.l[1].to_le_bytes());
         dst[16..24].copy_from_slice(&p.l[2].to_le_bytes());
         dst[24..32].copy_from_slice(&p.l[3].to_le_bytes());
-        self.update(&p);
+        self.update(p);
     }
 
     fn dec_partial(&mut self, dst: &mut [u8; 32], src: &[u8]) {
@@ -277,7 +261,7 @@ impl State {
                 u64::from_le_bytes(dst[24..32].try_into().unwrap()),
             ],
         };
-        self.update(&p);
+        self.update(p);
     }
 
     fn mac(&mut self, adlen: usize, mlen: usize) -> Tag {
@@ -292,7 +276,7 @@ impl State {
             s[4].l[3] ^= s[0].l[3];
         }
         for _ in 0..10 {
-            self.update(&t);
+            self.update(t);
         }
         let s = &mut self.s;
         s[0].l[0] ^= s[1].l[1] ^ (s[2].l[0] & s[3].l[0]);
